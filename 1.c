a25d73c9a3d2cc9c60421980b433470355aa434d
silen7broke <silen7broke@gmail.com>
2020-10-01 15:55:56 -0300

    finalizando programa 1

--- 1.c
+++ 1.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+void mostrarNaTela(int variavel);
+
 int main(){
-	printf("Hello, world!\n");
+	int variavel;
+	printf("Digite um numero para variavel:\n");
+	scanf("%d",&variavel);
+	system("pause");
+	mostrarNaTela(variavel);
+
 	return 0;
+}
+
+void mostrarNaTela(int numero){
+    int *ptr;
+    int i;
+    ptr=&numero;
+    printf("nome da variavel: numero\n");
+	printf("valor da variavel: %d\n",numero);
+	printf("endereco da variavel: %p\n",ptr);
+	printf("nome do ponteiro: ptr\n");
+    printf("endereco do ponteiro: %p\n",&ptr);
+    printf("conteudo apontado pelo ponteiro: %d\n",*ptr);
 }
